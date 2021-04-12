55096934333d320e415bd337874eea83c12144ca
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-04-11 23:52:36 -0600

    adding mutations and queries

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//update when the mongo server is set up
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
